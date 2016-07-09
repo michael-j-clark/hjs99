3ab2946aadce86026aaa4a0046c3533ecf63fc58
michael-j-clark <dev@0x03bb.com>
2016-07-09 12:33:43 -0500

    adding InsAt.hs and RemK.hs, 20 & 21

---
+++ src/21to30/RemK.hs
@@ -0,0 +1,8 @@
+module remK where
+
+-- P20  Remove the K'th element from a list. 
+
+removeAt :: [a] -> Int -> [a]  
+removeAt  y@(x:xs) k  
+    | k > 0 = take k y ++  drop (k + 1) y
+    | otherwise = error "k cannot be negative"
