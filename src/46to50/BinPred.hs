2bc73b6afe47296b11daa4a6a3cd0d2eabf517ad
michael-j-clark <michaelclark@abstractproduct.com>
2016-12-20 15:33:05 -0600

    initial commit

---
+++ src/46to50/BinPred.hs
@@ -0,0 +1,35 @@
+module BinPred where
+
+import Data.Bool
+
+--(**) Define predicates and/2, or/2, nand/2, nor/2, xor/2, impl/2 and equ/2 
+--(for logical equivalence) which succeed or fail according to the result of
+-- their respective operations; e.g. and(A,B) will succeed, if and only if both A and B succeed.
+
+--A logical expression in two variables can then be written as in the following example: and(or(A,B),nand(A,B)).
+
+--Now, write a predicate table/3 which prints the truth table of a given logical expression in two variables.
+
+--Example:
+
+--(table A B (and A (or A B)))
+--true true true
+--true fail true
+--fail true fail
+--fail fail fail
+
+and :: Bool -> Bool -> Bool
+and True True = True
+and  _   _    = False
+
+or :: Bool -> Bool  -> Bool
+or True  True   = True
+or True  False  = True
+or False True   = True
+or False False  = True
+
+nand :: Bool -> Bool -> Bool
+nand True True = False
+nand _    _    = True
+
+
