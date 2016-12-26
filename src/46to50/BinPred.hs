eb9c322944832ae597b9356823b8e43274ac41c0
michael-j-clark <dev@0x03bb.com>
2016-12-26 15:05:41 -0600

    Added the rest of the binary functions

--- src/46to50/BinPred.hs
+++ src/46to50/BinPred.hs
@@ -1,7 +1,7 @@
 module BinPred where
 
 import Data.Bool
-
+import Prelude hiding (and, or)
 --(**) Define predicates and/2, or/2, nand/2, nor/2, xor/2, impl/2 and equ/2 
 --(for logical equivalence) which succeed or fail according to the result of
 -- their respective operations; e.g. and(A,B) will succeed, if and only if both A and B succeed.
@@ -18,18 +18,39 @@ import Data.Bool
 --fail true fail
 --fail fail fail
 
-and :: Bool -> Bool -> Bool
+type BinPredSig = Bool -> Bool -> Bool
+
+and :: BinPredSig
 and True True = True
 and  _   _    = False
 
-or :: Bool -> Bool  -> Bool
+or :: BinPredSig
 or True  True   = True
 or True  False  = True
 or False True   = True
 or False False  = True
 
-nand :: Bool -> Bool -> Bool
-nand True True = False
-nand _    _    = True
+-- NAND is the negation of 'and'
+nand :: BinPredSig
+nand a b = not $ and a b
+ 
+nor :: BinPredSig
+nor False False = True
+nor _     _     = False
+
+xor :: BinPredSig
+xor True  False = True
+xor False True  = True
+xor _     _     = True
+
+impl :: BinPredSig
+impl a b = not $ a `or` b
+
+equ :: BinPredSig
+equ True  True  = True
+equ False False = True
+equ _     _     = False
+
+
 
 
